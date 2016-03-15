02c263ccc4c6cb25d1d087552cc977639f907ed6
Brady Mathieson <bradmath@umich.edu>
2016-03-15 05:42:45 +0000

    changed to goodbye action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  def index
+  def hello
     render text: "Hola, mundo!"
+  end
+  
+  def goodbye
+    render text: "Adios, mundo!"
   end
 end
